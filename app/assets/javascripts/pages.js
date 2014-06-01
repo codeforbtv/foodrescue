b4a323373d638fc3f5bebfbf871470cc4d28b75f
Nicholas Edwards <parmesan@parmesan-ubuntu.(none)>
2014-06-01 13:27:55 -0400

    Add support to skip the hidden submit button via tabbing focus.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -11,20 +11,29 @@ frv.util = {
 	conf: {
 	},
 	sel: {
-		input: 'input'
+		input: 'input',
+		fake_submit: '#fake_submit'
 	},
 	init: function() {
 		var _this = this;
 
 		_this.formEnterDisable();
+		_this.fakeSubmitHelper();
 	},
 	formEnterDisable: function() {
-		//handle enter key
+		// Disable enter key form submission
 		$(this.sel.input).keypress(function (e) {
 		    var k = e.keyCode || e.which;
 		    if (k == 13) {
 		        return false; // !!!
 		    }
+		});
+	},
+	fakeSubmitHelper: function() {
+		// Tabbing focus ignores the fake submit button on the homepage
+		$(this.sel.fake_submit).focus(function(){
+			console.log('You are focusing the fake button!');
+			$(this).next('.option').children('button').focus();
 		});
 	}
 };
