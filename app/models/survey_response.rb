ec173836b1b4e7b2e5a7937ccc242f2ef73cca54
Ben Glassman <ben@vtdesignworks.com>
2014-06-01 10:54:25 -0400

    Add is_edible? method to SurveyResponse

--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -4,4 +4,14 @@ class SurveyResponse < ActiveRecord::Base
   def default_values
     self.uuid = SecureRandom.uuid
   end
+
+  def edible?
+    edible = true
+
+    if opened or dangerous_temperature or old or distressed
+      edible = false
+    end
+
+    return edible
+  end
 end
